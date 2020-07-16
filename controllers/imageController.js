8a276c15cfcbdf65a76eceb9a840b2e4d26d1aba
楊宗翰 <feveraly@gmail.com>
2020-07-17 01:28:47 +0800

    image controller add ext

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -4,8 +4,15 @@ const config = require('../config.js')
 
 module.exports = {
     async upload (ctx) {
+        const {ext} = ctx.query
+
+        if (ext !== 'jpg' && ext !== 'png') {
+            ctx.response.status = 400
+            ctx.body = { message: 'query parameter should be "jpg" or "png"'}
+        }
+
         const imageId = shortUUID().generate()
-        fs.writeFileSync(`images/${imageId}.jpg`, ctx.file.buffer)
-        ctx.body = {image_url: `${config.serverBaseUrl}/${imageId}.jpg`}
+        fs.writeFileSync(`images/${imageId}.${ext}`, ctx.file.buffer)
+        ctx.body = {image_url: `${config.serverBaseUrl}/${imageId}.${ext}`}
     }
 }
