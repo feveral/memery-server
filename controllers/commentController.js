2300d34499f11dc5e5a98e438343b6563ad31043
楊宗翰 <feveraly@gmail.com>
2020-07-22 17:30:05 +0800

    comment api parameter check

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -4,7 +4,21 @@ const Comment = require('../models/comment.js')
 module.exports = {
 
     async getComments (ctx) {
-        const {meme_id, skip} = ctx.query
+        const {meme_id} = ctx.query
+        const {skip} = ctx.query
+
+        if (!meme_id) {
+            ctx.response.status = 400
+            ctx.body = { message: 'query parameter "meme_id" should be given.'}
+        }
+        try {
+            skip = parseInt(skip)
+        } catch (e) {
+            ctx.reponse.status = 400
+            ctx.body = { message: 'query parameter "skip" should be an integer.'}
+            return
+        }
+
         const comments = await Comment.find({memeId: meme_id, limit: 15, skip})
         ctx.body = comments
     },
@@ -12,6 +26,15 @@ module.exports = {
     async addComment(ctx) {
         const userId = ctx.user
         const {meme_id, content} = ctx.request.body
+
+        if (!meme_id) {
+            ctx.response.status = 400
+            ctx.body = { message: 'body parameter "meme_id" should be given.'}
+        } else if (!content) {
+            ctx.response.status = 400
+            ctx.body = { message: 'body parameter "content" should be given.'}
+        }
+
         const comment = await Comment.add(meme_id, userId, content)
         ctx.body = comment
     }
