b22fe62a521c87bc28297942191041ec8b1619dc
楊宗翰 <feveraly@gmail.com>
2020-07-25 02:01:41 +0800

    fix tag name check bug

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -3,15 +3,9 @@ const Tag = require('../models/tag.js')
 module.exports = {
 
     async getTags (ctx) {
-        const {name} = ctx.query
+        const name = ctx.query.name || ''
         const limit = parseInt(ctx.query.limit) || 10
         const skip = parseInt(ctx.query.skip) || 0
-
-        if (!name) {
-            ctx.response.status = 400
-            ctx.body = { message: 'query parameter "name" should be an given.'}
-            return
-        }
 
         const tags = await Tag.find({name, limit, skip})
         ctx.body = tags
