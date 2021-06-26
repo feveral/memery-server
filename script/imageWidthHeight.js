3a6610f87b35f7a20f9a3748a6df9ab9ccc68f6c
楊宗翰 <feveraly@gmail.com>
2021-06-26 13:50:19 +0800

    image width height script

---
+++ script/imageWidthHeight.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+const database = require('../database/database.js');
+const constants = require('../constants.js');
+const probe = require('probe-image-size');
+const { ObjectID } = require('mongodb');
+
+(
+    async () => {
+
+        const collection = await database.getCollection(constants.COLLECTION_IMAGE)
+        let isFinish = false
+        let counter = 0
+        while (!isFinish) {
+            
+            const images = await collection.find().skip(counter).limit(20).toArray()
+            for (let i = 0; i < images.length; i++) {
+                const image = images[i];
+                console.log("image", image)
+                try {
+                    const probeResult = await probe(images[i].url)
+                    console.log(probeResult)
+                    await collection.updateOne(
+                            {_id: ObjectID(image._id)},
+                            {$set: {width: probeResult.width, height: probeResult.height}}
+                    )
+                } catch (e) {
+                    console.log(`${e.name}: ${e.message}`)
+                    await collection.updateOne(
+                        {_id: ObjectID(image._id)},
+                        {$set: {width: 0, height: 0}}
+                    )
+                }
+                console.log('-'.repeat(process.stdout.columns))
+            }
+            counter += images.length
+            if (images.length === 0) isFinish = true
+        }
+    }
+)()
